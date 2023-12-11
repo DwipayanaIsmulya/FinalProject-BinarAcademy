4412b352bba72ad2b75c54d6c8d1e8adf709fbf1
Bintang Ary <86241827+Bintangaryw@users.noreply.github.com>
2023-12-11 19:36:58 +0700

    Bintang (#21)
    * Add: Layout Beranda Kelas Saya dan Beranda Topik Kelas
    
    * Add -> Card Komponen untuk Kelas dan mengedit navbar
    
    * Add -> Layout pembayaran dan Layout pembayaran selesai
    
    * Add -> API Integration untuk Admin Kelola Kelas dan Topik Kelas

--- src/Components/CardKelasComponent.jsx
+++ src/Components/CardKelasComponent.jsx
@@ -3,15 +3,16 @@ import levelBadge from "../assets/img/Card/levelBadge.png";
 import book from "../assets/img/Card/book.png";
 import time from "../assets/img/Card/time.png";
 import premium from "../assets/img/Card/premium.png";
+import PropType from "prop-types";
 
-const CardKelasComponent = () => {
+const CardKelasComponent = ({ name, level, price, isPremium, categoryId }) => {
   return (
     <>
       <div className="bg-white w-[350px] h-[225px] rounded-xl shadow-xl md:w-[440px] md:h-[280px]">
         <div className=" bg-gray-500 w-full h-[40%] rounded-t-xl"></div>
         <div className="p-2">
           <div className="flex justify-between w-full">
-            <div className="font-medium text-[#6148FF] md:text-xl">UI/UX Design</div>
+            <div className="font-medium text-[#6148FF] md:text-xl">{categoryId}</div>
             <div className="flex">
               <div className=" my-auto">
                 <img src={star} className="" />
@@ -19,14 +20,14 @@ const CardKelasComponent = () => {
               <div>4.7</div>
             </div>
           </div>
-          <div className="font-medium md:text-xl">Belajar Web Design dengan Figma</div>
+          <div className="font-medium md:text-xl">{name}</div>
           <div className="font-medium text-sm md:pt-2">By Angela Doe</div>
           <div className="flex justify-between md:py-1">
             <div className="flex text-sm">
               <div className="my-auto pr-1">
                 <img src={levelBadge} />
               </div>
-              <div className="text-sm font-medium text-[#6148FF]">Intermediate Level</div>
+              <div className="text-sm font-medium text-[#6148FF]">{level} Level</div>
             </div>
             <div className="flex text-sm">
               <div className="my-auto pr-1">
@@ -42,17 +43,24 @@ const CardKelasComponent = () => {
             </div>
           </div>
           <div className="py-1">
-            <div className="bg-[#489CFF] flex w-fit px-3 rounded-xl">
+            <div className={`${isPremium ? "bg-[#489CFF] flex w-fit px-3 rounded-xl" : "bg-[#6148FF] flex w-fit px-3 rounded-xl"} `}>
               <div className="my-auto px-1">
-                <img src={premium} />
+                <img src={isPremium ? premium : null} />
               </div>
-              <div className="text-white">Premium</div>
+              <div className="text-white">{isPremium ? `Rp ${price}` : "Mulai Kelas"}</div>
             </div>
           </div>
         </div>
       </div>
     </>
   );
+};
+CardKelasComponent.propTypes = {
+  name: PropType.string,
+  level: PropType.string,
+  price: PropType.any,
+  isPremium: PropType.any,
+  categoryId: PropType.any,
 };
 
 export default CardKelasComponent;
