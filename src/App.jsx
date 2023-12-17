917ca2af48cde82e8ee04223293ddf5e8e3832a1
bintang ary <wardhanabintang07@gmail.com>
2023-12-17 23:22:32 +0700

    Add -> Fitur login + Redux login

--- src/App.jsx
+++ src/App.jsx
@@ -14,32 +14,38 @@ import PembayaranDonePage from "./Pages/UserLogin/PembayaranDonePage";
 import MobileNavbar from "./Components/UserLogin/MobileNavbar";
 import ProgressCardKelasComponent from "./Components/ProgressCardKelasComponent";
 import EditProfileComponent from "./Components/UserLogin/EditProfileComponent";
+import LoginPage from "./Pages/Login/LoginPage";
+import { Provider } from "react-redux";
+import store from "./redux/store";
 
 function App() {
   return (
     <>
-      <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<Home />} />
+      <Provider store={store}>
+        <BrowserRouter>
+          <Routes>
+            <Route path="/" element={<Home />} />
 
-          {/* The page for not found url path */}
-          <Route path="*" element={<h1 className="d-flex flex-row justify-content-center align-items-center vh-100">404 Page Not Found</h1>} />
-          <Route path="adminkelolakelas" element={<KelolaKelasPage />} />
-          <Route path="admindashboard" element={<AdminDashboardPage />} />
-          <Route path="register" element={<Register />} />
-          <Route path="kelassaya" element={<BerandaKelasSayaPage />} />
-          <Route path="topikkelas" element={<BerandaTopikKelasPage />} />
-          <Route path="beranda" element={<Beranda />} />
-          <Route path="card" element={<CardKelasComponent />} />
-          <Route path="notifikasi" element={<NotifikasiPage />} />
-          <Route path="akun" element={<AkunPage />} />
-          <Route path="pembayaran" element={<PembayaranPage />} />
-          <Route path="pembayarandone" element={<PembayaranDonePage />} />
-          <Route path="mobilenavbar" element={<MobileNavbar />} />
-          <Route path="progresscard" element={<ProgressCardKelasComponent />} />
-          <Route path="editprofile" element={<EditProfileComponent />} />
-        </Routes>
-      </BrowserRouter>
+            {/* The page for not found url path */}
+            <Route path="*" element={<h1 className="d-flex flex-row justify-content-center align-items-center vh-100">404 Page Not Found</h1>} />
+            <Route path="adminkelolakelas" element={<KelolaKelasPage />} />
+            <Route path="admindashboard" element={<AdminDashboardPage />} />
+            <Route path="register" element={<Register />} />
+            <Route path="kelassaya" element={<BerandaKelasSayaPage />} />
+            <Route path="topikkelas" element={<BerandaTopikKelasPage />} />
+            <Route path="beranda" element={<Beranda />} />
+            <Route path="card" element={<CardKelasComponent />} />
+            <Route path="notifikasi" element={<NotifikasiPage />} />
+            <Route path="akun" element={<AkunPage />} />
+            <Route path="pembayaran" element={<PembayaranPage />} />
+            <Route path="pembayarandone" element={<PembayaranDonePage />} />
+            <Route path="mobilenavbar" element={<MobileNavbar />} />
+            <Route path="progresscard" element={<ProgressCardKelasComponent />} />
+            <Route path="editprofile" element={<EditProfileComponent />} />
+            <Route path="login" element={<LoginPage />} />
+          </Routes>
+        </BrowserRouter>
+      </Provider>
     </>
   );
 }
