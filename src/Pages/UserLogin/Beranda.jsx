e7d8ee8a451d911297cebb1eb25d0235ef6b7a85
Bintang Ary <86241827+Bintangaryw@users.noreply.github.com>
2023-12-28 10:35:36 +0700

    Bintang (#37)
    * Add: Layout Beranda Kelas Saya dan Beranda Topik Kelas
    
    * Add -> Card Komponen untuk Kelas dan mengedit navbar
    
    * Add -> Layout pembayaran dan Layout pembayaran selesai
    
    * Add -> API Integration untuk Admin Kelola Kelas dan Topik Kelas
    
    * Add -> Halaman notifikasi, Mobile navbar, Progress card. Fix -> Kelas card
    
    * Fix -> Mobile responsive halaman Kelas Saya dan Topik Kelas
    
    * Add -> Layout pengaturan akun
    
    * Add -> Fitur login + Redux login
    
    * Add -> Fitur login + redux login
    
    * Warning fix -> Register.jsx dan Beranda.jsx
    
    * Add -> Filter kategori di Topik Kelas
    
    * Add -> fitur logout, Fix -> Login redux bug
    
    * Migrasi ke Redux -> Course topik kelas
    
    * Memperbarui navbar dengan logo Digilearn Academy
    
    * Topik kelas Link to Detail kelas by courseId
    
    * API integerasi untuk Profile

--- src/Pages/UserLogin/Beranda.jsx
+++ src/Pages/UserLogin/Beranda.jsx
@@ -107,9 +107,7 @@ const Beranda = () => {
         filtered = filtered.filter((course) => course.category === "UI/UX");
       }
       if (android) {
-        filtered = filtered.filter(
-          (course) => course.category === "Web Development"
-        );
+        filtered = filtered.filter((course) => course.category === "Web Development");
       }
       if (backend) {
         filtered = filtered.filter((course) => course.category === "Back-End");
@@ -118,14 +116,10 @@ const Beranda = () => {
         filtered = filtered.filter((course) => course.category === "Front-End");
       }
       if (business) {
-        filtered = filtered.filter(
-          (course) => course.category === "Business Intelligence"
-        );
+        filtered = filtered.filter((course) => course.category === "Business Intelligence");
       }
       if (ios) {
-        filtered = filtered.filter(
-          (course) => course.category === "IOS Development"
-        );
+        filtered = filtered.filter((course) => course.category === "IOS Development");
       }
 
       setFilteredCourses(filtered);
@@ -144,14 +138,11 @@ const Beranda = () => {
         const token = localStorage.getItem("token");
         if (!token) return;
 
-        const response = await axios.get(
-          "https://fpbejs-production.up.railway.app/api/v1/auth/me",
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
+        const response = await axios.get("https://fpbejs-production.up.railway.app/api/v1/auth/me", {
+          headers: {
+            Authorization: `Bearer ${token}`,
+          },
+        });
 
         const { data } = response.data;
 
@@ -184,18 +175,10 @@ const Beranda = () => {
           {/* left Nav */}
           <div className="flex w-[60%] h-full">
             <div className="flex md:justify-center md:items-center items-center h-full w-[100%] sm:w-[30%]">
-              <img
-                className="flex ms-6 mt-1 w-[80%] md:w-[70%]"
-                src={digilearn_align}
-                alt=""
-              />
+              <img className="flex ms-6 mt-1 w-[80%] md:w-[70%]" src={digilearn_align} alt="" />
             </div>
             <div className="w-[20%] flex items-center flex-initial sm:visible md:w-[70%]">
-              <input
-                className="hidden py-5 px-8 w-[526px] h-[62px] rounded-2xl md:flex md:text-md md:visible"
-                type="text"
-                placeholder="Cari kursus terbaik.."
-              />
+              <input className="hidden py-5 px-8 w-[526px] h-[62px] rounded-2xl md:flex md:text-md md:visible" type="text" placeholder="Cari kursus terbaik.." />
             </div>
           </div>
           {/* Right Nav */}
@@ -211,11 +194,7 @@ const Beranda = () => {
                       color: "white",
                     }}
                   >
-                    <img
-                      className="flex w-[20px] h-[20px]"
-                      src={loginImage}
-                      alt=""
-                    />
+                    <img className="flex w-[20px] h-[20px]" src={loginImage} alt="" />
                     <p>&nbsp; Profile</p>
                   </Link>
                 </>
@@ -228,11 +207,7 @@ const Beranda = () => {
                     color: "white",
                   }}
                 >
-                  <img
-                    className="flex w-[20px] h-[20px]"
-                    src={loginImage}
-                    alt=""
-                  />
+                  <img className="flex w-[20px] h-[20px]" src={loginImage} alt="" />
                   <p>&nbsp; Masuk</p>
                 </Link>
               )}
@@ -245,11 +220,7 @@ const Beranda = () => {
       <div className="flex relative flex-wrap flex-initial w-[100%] h-[300px]">
         {/* left main-content */}
         <div className="flex">
-          <img
-            className="absolute top-0 right-0 left-0 bottom-0"
-            src={mainImage}
-            alt=""
-          />
+          <img className="absolute top-0 right-0 left-0 bottom-0" src={mainImage} alt="" />
         </div>
         {/* right main-content */}
         <div className="flex md:z-10 w-[100%] items-center justify-center pt-14 flex-initial md:bg-gradient-to-l from-[#6148FF] from-35% to-85%">
@@ -257,9 +228,7 @@ const Beranda = () => {
             <h1 className="my-1">Belajar</h1>
             <h1 className="my-1">dari Praktisi Terbaik!</h1>
             <Link to="/topikkelas">
-              <button className="my-1 bg-[#fff] w-[250px] font-bold text-[#6148FF] rounded-xl p-1">
-                IKUTI KELAS
-              </button>
+              <button className="my-1 bg-[#fff] w-[250px] font-bold text-[#6148FF] rounded-xl p-1">IKUTI KELAS</button>
             </Link>
           </div>
         </div>
@@ -275,51 +244,27 @@ const Beranda = () => {
         </div>
         <div className="flex flex-wrap justify-around flex-col-2 flex-initial w-[80%] h-full mb-5">
           <div className="flex flex-col items-center justify-center w-[160px] h-[136px] md:m-1 hover:scale-105 duration-300">
-            <img
-              className="flex w-[140px] h-[100px] rounded-2xl"
-              src={uiux}
-              alt=""
-            />
+            <img className="flex w-[140px] h-[100px] rounded-2xl" src={uiux} alt="" />
             <div className="flex text-l font">UI-UX</div>
           </div>
           <div className="flex flex-col items-center justify-center w-[160px] h-[136px] md:m-1 hover:scale-105 duration-300">
-            <img
-              className="flex w-[140px] h-[100px] rounded-2xl"
-              src={productManagement}
-              alt=""
-            />
+            <img className="flex w-[140px] h-[100px] rounded-2xl" src={productManagement} alt="" />
             <div className="flex text-l font">Front-End</div>
           </div>
           <div className="flex flex-col items-center justify-center w-[160px] h-[136px] md:m-1 hover:scale-105 duration-300">
-            <img
-              className="flex w-[140px] h-[100px] rounded-2xl"
-              src={webdev}
-              alt=""
-            />
+            <img className="flex w-[140px] h-[100px] rounded-2xl" src={webdev} alt="" />
             <div className="flex text-l font">Back-End</div>
           </div>
           <div className="flex flex-col items-center justify-center w-[160px] h-[136px] md:m-1 hover:scale-105 duration-300">
-            <img
-              className="flex w-[140px] h-[100px] rounded-2xl"
-              src={androidDev}
-              alt=""
-            />
+            <img className="flex w-[140px] h-[100px] rounded-2xl" src={androidDev} alt="" />
             <div className="flex text-l font">Android Development</div>
           </div>
           <div className="flex flex-col items-center justify-center w-[160px] h-[136px] md:m-1 hover:scale-105 duration-300">
-            <img
-              className="flex w-[140px] h-[100px] rounded-2xl"
-              src={iosdev}
-              alt=""
-            />
+            <img className="flex w-[140px] h-[100px] rounded-2xl" src={iosdev} alt="" />
             <div className="flex text-l font">IOS Development</div>
           </div>
           <div className="flex flex-col items-center justify-center w-[160px] h-[136px] md:m-1 hover:scale-105 duration-300">
-            <img
-              className="flex w-[140px] h-[100px] rounded-2xl"
-              src={dataImg}
-              alt=""
-            />
+            <img className="flex w-[140px] h-[100px] rounded-2xl" src={dataImg} alt="" />
             <div className="flex text-l font">Data Science</div>
           </div>
         </div>
@@ -335,91 +280,45 @@ const Beranda = () => {
         </div>
         <div className="flex justify-center flex-wrap flex-initial w-[100%]">
           <div>
-            <button
-              onClick={handleAll}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                all ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleAll} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${all ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               All
             </button>
           </div>
           <div>
-            <button
-              onClick={handleFrontend}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                frontend ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleFrontend} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${frontend ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               Front-End
             </button>
           </div>
           <div>
-            <button
-              onClick={handleDesign}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                design ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleDesign} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${design ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               UI/UX Design
             </button>
           </div>
           <div>
-            <button
-              onClick={handleBackend}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                backend ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleBackend} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${backend ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               Back-End
             </button>
           </div>
           <div>
-            <button
-              onClick={handleAndroid}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                android ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleAndroid} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${android ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               Android Development
             </button>
           </div>
           <div>
-            <button
-              onClick={handleIos}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                ios ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleIos} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${ios ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               IOS Development
             </button>
           </div>
           <div>
-            <button
-              onClick={handleBusiness}
-              className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${
-                business ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"
-              }`}
-            >
+            <button onClick={handleBusiness} className={`w-auto h-[24px] m-1 px-4 md:mx-2 rounded-2xl ${business ? "bg-[#6148FF] text-white" : "bg-[#E8F1FF]"}`}>
               Business Intelligence
             </button>
           </div>
         </div>
         <div className="md:grid md:grid-cols-2 md:gap-4 flex flex-wrap justify-center items-center w-[100%] md:w-[80%] mt-4">
           {filteredCourses.map((course) => (
-            <div
-              className="flex justify-center items-center w-auto hover:scale-105 duration-300"
-              key={course.id}
-            >
-              <CardKelasComponent
-                id={course.id}
-                name={course.courseCode}
-                level={course.level}
-                price={course.price}
-                isPremium={course.isPremium}
-                category={course.category}
-                rating={course.rating}
-              />
+            <div className="flex justify-center items-center w-auto hover:scale-105 duration-300" key={course.id}>
+              <CardKelasComponent id={course.id} name={course.name} level={course.level} price={course.price} isPremium={course.isPremium} category={course.category} rating={course.rating} />
             </div>
           ))}
         </div>
