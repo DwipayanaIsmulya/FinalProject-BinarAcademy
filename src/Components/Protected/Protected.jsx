9395331259e23516515bf4e6d8c2747e4d84948a
bintang ary <wardhanabintang07@gmail.com>
2023-12-29 20:51:03 +0700

    Fix toast for login

--- src/Components/Protected/Protected.jsx
+++ src/Components/Protected/Protected.jsx
@@ -14,31 +14,16 @@ const Protected = ({ children }) => {
           return navigate("/login");
         }
 
-<<<<<<< HEAD
         await axios.get(`https://fpbejs-production.up.railway.app/api/v1/auth/me`, {
           headers: {
             Authorization: `Bearer ${token}`,
           },
         });
-=======
-        await axios.get(
-          `https://fpbejs-production.up.railway.app/api/v1/auth/me`,
-          {
-            headers: {
-              Authorization: `Bearer ${token}`,
-            },
-          }
-        );
->>>>>>> 20cefc9dccc662830b3ca5fc3bae1668e5edb039
       } catch (error) {
         // Handle error as needed
         console.error("Error fetching user data:", error);
         // Optionally, you can redirect to login on error as well
-<<<<<<< HEAD
-        // navigate("/login");
-=======
         navigate("/login");
->>>>>>> 20cefc9dccc662830b3ca5fc3bae1668e5edb039
       }
     };
 
